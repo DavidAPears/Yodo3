974f9f621c2d6fd4a50c794893e577802b396d69
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-22 01:05:40 +0100

    neatens presentation of book genre

--- src/main/java/models/enums/Genre.java
+++ src/main/java/models/enums/Genre.java
@@ -2,25 +2,32 @@ package models.enums;
 
 public enum Genre {
 
-    ADVENTURE,
-    FANTASY,
-    HUMOUR,
-    SCIFI,
-    EDUCATIONAL,
-    ROMANCE,
-    HISTORICAL,
-    HORROR,
-    CLASSIC,
-    POETRY,
-    SCHOOL,
-    BIOGRAPHY,
-    ANIMALS,
-    ART,
-    CRAFTS_AND_HOBBIES,
-    CRIME,
-    GENERAL,
-    TRAVEL,
-    REFERENCE,
-    OTHER
+    ADVENTURE("Adventure"),
+    ANIMALS("Animals"),
+    ART("Art"),
+    BIOGRAPHY("Biography"),
+    CLASSIC("Classic"),
+    CRAFTS_AND_HOBBIES("Crafts and hobbies"),
+    CRIME("Crime"),
+    EDUCATIONAL("Educational"),
+    FANTASY("Fantasy"),
+    GENERAL("General"),
+    HISTORICAL("Historical"),
+    HORROR("Horror"),
+    HUMOUR("Humour"),
+    POETRY("Poetry"),
+    REFERENCE("Reference"),
+    ROMANCE("Romance"),
+    SCHOOL("School"),
+    SCIFI("Science fiction"),
+    TRAVEL("Travel"),
+    OTHER("Other");
+
+    private String name;
+
+    Genre(String name) { this.name = name; }
+
+    public String getName() { return this.name; }
+
 
 }
