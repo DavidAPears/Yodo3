778b693ad97552792053d83ab25eb61b0506fe90
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-22 01:11:28 +0100

    neatens presentation of game types

--- src/main/java/models/enums/Format.java
+++ src/main/java/models/enums/Format.java
@@ -2,9 +2,16 @@ package models.enums;
 
 public enum Format {
 
-    HARDBACK,
-    PAPERBACK,
-    BOARDBOOK,
-    OTHER
+    HARDBACK("Hardback"),
+    PAPERBACK("Paperback"),
+    BOARDBOOK("Board book"),
+    OTHER("Other");
+
+    private String name;
+
+    Format(String name) { this.name = name; }
+
+    public String getName() { return this.name; }
+
 
 }
--- src/main/java/models/enums/GameType.java
+++ src/main/java/models/enums/GameType.java
@@ -2,16 +2,23 @@ package models.enums;
 
 public enum GameType {
 
-    ACTION,
-    HISTORICAL,
-    WAR,
-    FANTASY,
-    MURDER_MYSTERY,
-    STRATEGY,
-    WORD,
-    LUCK,
-    SCIFI,
-    NUMBER,
-    OTHER
+    ACTION("Action"),
+    FANTASY("Fantasy"),
+    HISTORICAL("Historical"),
+    LUCK("Luck"),
+    MURDER_MYSTERY("Murder mystery"),
+    NUMBER("Number"),
+    SCIFI("Science fiction"),
+    STRATEGY("Strategy"),
+    WAR("War"),
+    WORD("Word"),
+    OTHER("Other");
+
+    private String name;
+
+    GameType(String name) { this.name = name; }
+
+    public String getName() { return this.name; }
+
 
 }
