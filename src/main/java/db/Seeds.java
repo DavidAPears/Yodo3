8c327ceeb3fd2aaa9e7793c10b96a6adaa9e7ddc
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-23 22:22:00 +0100

    adds several seeds (total now 20)

--- src/main/java/db/Seeds.java
+++ src/main/java/db/Seeds.java
@@ -37,26 +37,46 @@ public class Seeds {
         DBHelper.save(computerGame1);
         ComputerGame computerGame2 = new ComputerGame("Mario Bros.", "Mario and Luigi in the sewers!", 6, "/images/mariobros.jpeg", user2, Console.SUPER_NES, AgeClassification.THREE, GameType.ACTION);
         DBHelper.save(computerGame2);
-        ComputerGame computerGame3 = new ComputerGame("Men in Black", "Mr Smith and Mr Jones battle the aliens in this fun game.", 8, "/images/meninblack.jpg", user2, Console.SUPER_NES, AgeClassification.SEVEN, GameType.SCIFI);
+        ComputerGame computerGame3 = new ComputerGame("Men in Black", "Mr Smith and Mr Jones battle the aliens in this fun game. (1 of 2 copies for sale.)", 8, "/images/meninblack.jpg", user2, Console.SUPER_NES, AgeClassification.SEVEN, GameType.SCIFI);
         DBHelper.save(computerGame3);
+        ComputerGame computerGame4 = new ComputerGame("Men in Black", "Mr Smith and Mr Jones battle the aliens in this fun game. (2 of 2 copies for sale.)", 8, "/images/meninblack.jpg", user2, Console.SUPER_NES, AgeClassification.SEVEN, GameType.SCIFI);
+        DBHelper.save(computerGame4);
+        ComputerGame computerGame5 = new ComputerGame("Men in Black II: Alien Escape", "You are MiB agent Agent K or Agent J, and are required to stop aliens blowing up the Earth with their Class 7 Ozone Demogrifier. Perfect for a relaxing Sunday afternoon.", 9, "/images/meninblackii.jpg", user2, Console.PLAYSTATION_2, AgeClassification.SEVEN, GameType.SCIFI);
+        DBHelper.save(computerGame5);
+        ComputerGame computerGame6 = new ComputerGame("ZTYPE", "Want a place at CodeClan? You have to get this game! It's free on the Internet, but so what?!", 400, "/images/ztype.png", user3, Console.PLAYSTATION_2, AgeClassification.EIGHTEEN, GameType.WORD);
+        DBHelper.save(computerGame6);
 
         Advert advert1 = new Advert("Huddersfield Town top, age 3-4 years", "Blue and white home replica shirt from 2017-18 season.", 10, Category.CLOTHING, "/images/htfc.jpeg", user3);
         DBHelper.save(advert1);
         Advert advert2 = new Advert("drumsticks", "Drumsticks. For banging drums.", 7, Category.MUSIC,"/images/drumsticks.jpg", user4);
         DBHelper.save(advert2);
+        Advert advert3 = new Advert("Manuel Rodriguez classical guitar", "Beautiful sound!", 100, Category.MUSIC,"/images/guitar.jpeg", user4);
+        DBHelper.save(advert3);
+        Advert advert4 = new Advert("Spider-man costume, age 3-4 years", "With great power comes a great costume.", 6, Category.CLOTHING, "/images/spiderman.jpeg", user3);
+        DBHelper.save(advert4);
+        Advert advert5 = new Advert("Batman costume, age 2-4 years", "Just the thing to wear when your little one is helping David and Edward with their Git merges.", 5, Category.CLOTHING, "/images/batman.jpeg", user5);
+        DBHelper.save(advert5);
+        Advert advert6 = new Advert("Elmo!", "One owner, much loved. Genuine reason for sale (I prefer Cookie Monster now).", 4, Category.TOY, "/images/elmo.jpeg", user6);
+        DBHelper.save(advert6);
 
-        Bicycle bicycle1 = new Bicycle("Pink bike", "Raleigh bike with stabilizers", 20, "/images/pinkbike.png", user5, 14, 3, 5);
+        Bicycle bicycle1 = new Bicycle("Pink bike", "Raleigh bike with stabilizers", 20, "/images/pinkbike.jpg", user5, 14, 3, 5);
         DBHelper.save(bicycle1);
-        Bicycle bicycle2 = new Bicycle("Purple bike", "Raleigh bike with basket", 25, "/images/purplebike.JPG", user5, 18, 6, 8);
+        Bicycle bicycle2 = new Bicycle("Purple bike", "The Raleigh Krush is the perfect cycling companion for any style conscious child, with a beautiful heart design, soft floating feathers and using the latest colour trends it provides instant riding style.", 25, "/images/purplebike.jpeg", user5, 18, 6, 8);
         DBHelper.save(bicycle2);
-        Bicycle bicycle3 = new Bicycle("Black bike", "Apollo bike with 12 gears", 30, "/images/blackbike.jpg", user5, 20, 7, 9);
+        Bicycle bicycle3 = new Bicycle("Black bike", "Apollo bike", 30, "/images/blackbikeapollo.jpg", user5, 20, 7, 9);
         DBHelper.save(bicycle3);
 
         Book book1 = new Book("Harry Potter and the Philosopher's Stone", "Book by J. K. Rowling", 5, "/images/harrypotter.jpeg", user6, Genre.FANTASY, Format.HARDBACK);
         DBHelper.save(book1);
+        Book book2 = new Book("The Railway Series", "Box of 26 classic books by the Rev. W Awdry. The Fat Controller pre-dates Intellij.", 20, "/images/railwayseries.jpeg", user6, Genre.TRAVEL, Format.HARDBACK);
+        DBHelper.save(book2);
+        Book book3 = new Book("The Tiger Who Came to Tea", "He had a good appetite actually. By Judith Kerr", 3, "/images/tiger.jpeg", user7, Genre.ANIMALS, Format.PAPERBACK);
+        DBHelper.save(book3);
 
         BoardGame boardGame1 = new BoardGame("Catan", "Catan board game, good condition", 12, "/images/catan.jpeg", user7, GameType.STRATEGY);
         DBHelper.save(boardGame1);
+        BoardGame boardGame2 = new BoardGame("Scrabble Junior", "Word game for kids.", 7, "/images/scrabble.jpeg", user8, GameType.WORD);
+        DBHelper.save(boardGame2);
 
     }
 
