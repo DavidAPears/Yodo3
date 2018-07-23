4cf2deb7f7efbdc8d149221b6c87a28065a677ab
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-23 15:20:33 +0100

    disables creating generic advert with category equal to subclasses

--- src/main/java/models/enums/Category.java
+++ src/main/java/models/enums/Category.java
@@ -2,21 +2,23 @@ package models.enums;
 
 public enum Category {
 
-    BICYCLE("Bicycle"),
-    BOARD_GAME("Board game"),
-    BOOK("Book"),
-    CD("CD"),
-    CLOTHING("Clothing"),
-    COMPUTER_GAME("Computer game"),
-    MUSIC("Music"),
-    TOY("Yoy"),
-    OTHER("Other");
+    BICYCLE("Bicycle", true),
+    BOARD_GAME("Board game", true),
+    BOOK("Book", true),
+    CD("CD", false),
+    CLOTHING("Clothing", false),
+    COMPUTER_GAME("Computer game", true),
+    MUSIC("Music", false),
+    TOY("Toy", false),
+    OTHER("Other", false);
 
     private String name;
+    private boolean isClass;
 
-    Category(String name) { this.name = name; }
+    Category(String name, boolean isClass) { this.name = name; this.isClass = isClass; }
 
     public String getName() { return this.name; }
+    public boolean isClass() { return this.isClass; }
 
 
 }
